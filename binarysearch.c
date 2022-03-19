b15bc84fc7704f910c6992a1559f4a7cb856b5a8
Popuri Keerthika <79918843+keerthikapopuri@users.noreply.github.com>
2022-03-19 22:25:40 +0530

    Update binarysearch.c

--- binarysearch.c
+++ binarysearch.c
@@ -1,38 +1,45 @@
-#include <stdio.h>  
-int binarySearch(int a[], int beg, int end, int val)    
-{    
-    int mid;    
-    if(end >= beg)     
-    {        mid = (beg + end)/2;    
-/* if the item to be searched is present at middle */  
-        if(a[mid] == val)    
-        {                 
-            return mid+1;    
-        }    
-            /* if the item to be searched is smaller than middle, then it can only be in left subarray */  
-        else if(a[mid] < val)     
-        {  
-            return binarySearch(a, mid+1, end, val);    
-        }   
-        else     
-        {  
-            return binarySearch(a, beg, mid-1, val);    
-        }          
-    }    
-    return -1;     
-}   
-int main() {  
-  int a[] = {11, 14, 25, 30, 40, 41, 52, 57, 70}; // given array  
-  int val = 40; // value to be searched  
-  int n = sizeof(a) / sizeof(a[0]); // size of array  
-  int res = binarySearch(a, 0, n-1, val); // Store result  
-  printf("The elements of the array are - ");  
-  for (int i = 0; i < n; i++)  
-  printf("%d ", a[i]);   
-  printf("\nElement to be searched is - %d", val);  
-  if (res == -1)  
-  printf("\nElement is not present in the array");  
-  else  
-  printf("\nElement is present at %d position of array", res);  
-  return 0;  
-}  
+#include<stdio.h>
+int i,a[20],n,x,flag=0;
+void bs();
+void main()
+{
+    printf("enter array size");
+    scanf("%d",&n);
+    printf("enter array ele");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("enter ele to be searched");
+    scanf("%d",&x);
+    bs();
+    if(flag==1)
+    {
+        printf("ele is found");
+    }
+    else
+    {
+        printf("ele is not found");
+    }
+}
+void bs()
+{
+    int low=0,high=n-1,mid;
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        if(a[mid]==x)
+        {
+            flag=1;
+            break;
+        }
+        else if(x<a[mid])
+        {
+            high=mid-1;
+        }
+        else
+        {
+            low=mid+1;
+        }
+    }
+}
